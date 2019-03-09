27d0c4a8e2d6933ad1825508b4b1d330b66a4902
jalal8068 <jalalahamad9@gmail.com>
2019-03-09 16:01:25 +0530

    1st commit by geetha

--- src/com/sample/SampleTest.java
+++ src/com/sample/SampleTest.java
@@ -2,7 +2,7 @@ package com.sample;
 
 public class SampleTest {
 public static void main(String[] args) {
-System.out.println("Line1");	
+System.out.println("Line121");	
 System.out.println("Line2");
 System.out.println("Line3");
 }
