0da2134c764f5bae3e17e963b3021275a7b4ed3b
jalal8068 <jalalahamad9@gmail.com>
2019-03-09 15:51:43 +0530

    1st message

---
+++ src/com/sample/SampleTest.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class SampleTest {
+public static void main(String[] args) {
+System.out.println("Line1");	
+System.out.println("Line2");
+System.out.println("Line3");
+}
+}
